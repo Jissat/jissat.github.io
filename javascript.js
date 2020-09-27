6628a02c510cd5fd537655ef31101395e4f40965
Jissat <43492877+Jissat@users.noreply.github.com>
2020-09-27 17:17:27 +0530

    p

--- javascript.js
+++ javascript.js
@@ -7,3 +7,36 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+function myFunction2() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function myFunction3() {
+
+  var x = document.getElementById("Demo2");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+	
+
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}function myFunction4() {
+  var x = document.getElementById("Demo3");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}function myFunction5() {
+  var x = document.getElementById("Demo4");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
