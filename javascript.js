69ff78b374333ea9f8f5bd731f7a8a052a4c0784
Jissat <43492877+Jissat@users.noreply.github.com>
2020-09-28 18:24:06 +0530

    l

--- javascript.js
+++ javascript.js
@@ -7,36 +7,22 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
-function myFunction2() {
-  var x = document.getElementById("Demo");
+
+function showing(i) {
+  var x = document.getElementById("Demo"+i);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
   } else {
     x.className = x.className.replace(" w3-show", "");
   }
 }
-function myFunction3() {
 
-  var x = document.getElementById("Demo2");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-	
+function hiding(j) {
 
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}function myFunction4() {
-  var x = document.getElementById("Demo3");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
-}function myFunction5() {
-  var x = document.getElementById("Demo4");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
+  var i;
+	for (i = 1; i <= 4; i++){
+	var x = document.getElementById("Demo"+i);
+	if(i!=j){
+	x.className = x.className.replace(" w3-show", "");}
   }
 }
